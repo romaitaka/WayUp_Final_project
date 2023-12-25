baa09831d02cba9194fc6b9c6f9bd731c1d8ddf2
romaitaka <trade.interes@gmail.com>
2023-12-25 16:32:45 +0300

    Restructure content ways

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
       var loader = document.querySelector(".loader-wrapper");
       loader.style.display = "none"; // Скрыть прелоудер
-    }, 1800); // Задержка в 5 секунд перед скрытием прелоудера
+    }, 1400); // Задержка в 5 секунд перед скрытием прелоудера
   });
   
