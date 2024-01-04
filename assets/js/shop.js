d6c26b1558a6772700fbe745456f5117c202a625
romaitaka <trade.interes@gmail.com>
2024-01-04 19:12:04 +0300

    card:hover

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -19,3 +19,39 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const products = document.querySelectorAll('.card-product');
+  const pageCount = document.querySelector('.shop__product-counter');
+  const maxProductsPerPage = 9;
+  const totalProducts = products.length;
+  const pages = document.querySelectorAll('.shop__page');
+
+  pages.forEach(function(page, index) {
+    page.addEventListener('click', function(event) {
+      // Предотвращаем стандартное поведение ссылки
+      event.preventDefault();
+      // Удаляем класс active со всех страниц
+      pages.forEach(p => p.classList.remove('active'));
+      // Добавляем класс active к текущей странице
+      this.classList.add('active');
+      // Определяем количество товаров на текущей странице
+      const productCount = (index + 1) * maxProductsPerPage < totalProducts ? maxProductsPerPage : totalProducts % maxProductsPerPage;
+      // Обновляем текст счетчика товаров
+      pageCount.textContent = `Показано: ${productCount} из ${totalProducts} товаров`;
+      // Скрываем все товары
+      products.forEach(p => p.style.display = 'none');
+      // Отображаем товары для текущей страницы
+      const start = index * maxProductsPerPage;
+      const end = start + productCount;
+      for (let i = start; i < end; i++) {
+        if (products[i]) {
+          products[i].style.display = 'block';
+        }
+      }
+    });
+  });
+});
+
+
